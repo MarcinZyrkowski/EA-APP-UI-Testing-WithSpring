3a46cf0dbda32092a5f5f0e49ce043a1626ce2b7
mzyrkowski <marcinzyrkowski7@gmail.com>
2022-09-10 16:40:15 +0200

    ui testing and microservices

---
+++ src/test/java/com/ea/SpringBasic/SpringBasicApplicationTests.java
@@ -0,0 +1,30 @@
+package com.ea.SpringBasic;
+
+import com.ea.SpringBasic.pages.MainPage;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@SpringBootTest
+@Profile("!qa")
+class SpringBasicApplicationTests {
+
+    @Autowired
+    private MainPage mainPage;
+
+    @Value("${app.url}")
+    private String appUrl;
+
+    @Value("chrome,firefox,edge")
+    private List<String> browsers;
+
+    @Test
+    void performLoginTest() {
+        mainPage.PerformLogin();
+    }
+
+}
